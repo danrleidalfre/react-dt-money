e6ec72bfcbc4775132556e71be601be4c11ee0f0
Danrlei <danrleidalfre@gmail.com>
2024-01-23 23:46:33 -0300

    implement header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default.ts'
 import { GlobalStyle } from './styles/global.ts'
+import { Transactions } from './pages/Transactions'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <Transactions />
     </ThemeProvider>
   )
 }
